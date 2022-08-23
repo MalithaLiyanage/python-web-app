5017c710f14d635fbfc6b50c5efbdb7ebbc61980
MalithaLiyanage <malitharanu82@gmail.com>
2022-08-23 19:24:50 +0530

    Connected with back end and displayed a result in web page

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from cgitb import html;
-from flask import Flask, render_template;
+from flask import Flask, render_template, request;
 
 # __name__ is the name of the app
 app = Flask(__name__)
@@ -7,5 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def home_post():
+    length = request.form['length']
+    width = request.form['width']
+    height = request.form['height']
+    volume = float(length) * float(width) * float(height)
+    return render_template('index.html', volume=volume)
 
 app.run('127.0.0.1')
